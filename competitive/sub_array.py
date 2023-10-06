378784920ffda3af16b256ef351af38e2338f848
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-10-06 22:57:03 +0545

    check if any number is repeated in an array

--- competitive/sub_array.py
+++ competitive/sub_array.py
@@ -1,15 +1,17 @@
 #Function to find a continuous sub-array which adds up to a given number.
 class Solution:
     def subArraySum(self,arr, n, s):
-        left = 0
-        sum = 0
         for i in range(n):
-            sum += arr[i]
-            if sum > s:
-                sum -= arr[left]
-                left += 1
-            if sum == s:
-                return [left+1, i+1]
+            sub_arr = arr[i:]
+            sum = 0
+            left = 0
+            for j, num in enumerate(sub_arr):
+                sum += num
+                if sum > s:
+                    sum -= sub_arr[left]
+                    left += 1
+                if sum == s:
+                    return [i+1, i+j+1]
         return [-1]
 
 sln = Solution()
@@ -17,3 +19,22 @@ n = 42
 s = 468
 arr = [135, 101, 170, 125, 79, 159, 163, 65, 106, 146, 82, 28, 162, 92, 196, 143, 28, 37, 192, 5, 103, 154, 93, 183, 22, 117, 119, 96, 48, 127, 172, 139, 70, 113, 68, 100, 36, 95, 104, 12, 123, 134]
 print(sln.subArraySum(arr, n, s))
+
+class Solution1:
+    def duplicates(self, arr, n): 
+    	# code here
+        arr2 = []
+        arr3 = []
+        for num in arr:
+            if len(arr2) == 0:
+                arr2.append(num)
+            elif num in arr2:
+                if num not in arr3:
+                    arr3.append(num)
+            else:
+                arr2.append(num)
+        if len(arr3) == 0:
+            return [-1]
+        else:
+            arr3.sort(reverse=False)
+            return arr3
