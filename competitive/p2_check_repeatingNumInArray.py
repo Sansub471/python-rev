97b6a12151b5eef3b3d401261d0550b60becb1cc
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-10-06 23:17:42 +0545

    check repeating number in an array.

---
+++ competitive/p2_check_repeatingNumInArray.py
@@ -0,0 +1,45 @@
+# Given an array a of size N which contains elements from 0 to N-1, 
+# you need to find all the elements occurring more than once in the given array. 
+# Return the answer in ascending order. If no such element is found, return list containing [-1]. 
+
+# Example 1
+# Input:
+# N = 4
+# a[] = {0,3,1,2}
+# Output: 
+# -1
+# Explanation: 
+# There is no repeating element in the array. Therefore output is -1.
+
+# Example 2
+# Input:
+# N = 5
+# a[] = {2,3,1,2,3}
+# Output: 
+# 2 3 
+# Explanation: 
+# 2 and 3 occur more than once in the given array.
+
+class Solution:
+    def duplicates(self, arr, n):
+    	# First check all the values that are 
+    	# present in an array then go to that 
+    	# values as indexes and increment by 
+    	# the size of array
+        for i in range(0, n):
+            index = arr[i] % n
+            arr[index] += n
+    	# Now check which value 
+    	# exists more 
+    	# than once by dividing 
+    	# with the size 
+    	# of array 
+        flag = False
+        res = []
+        for i in range(0,n): 
+            if (arr[i]//n) > 1: 
+                res.append(i)
+                flag=True
+        if flag==False:
+            res.append(-1)
+        return res
--- competitive/sub_array.py
+++ competitive/sub_array.py
@@ -19,22 +19,3 @@ n = 42
 s = 468
 arr = [135, 101, 170, 125, 79, 159, 163, 65, 106, 146, 82, 28, 162, 92, 196, 143, 28, 37, 192, 5, 103, 154, 93, 183, 22, 117, 119, 96, 48, 127, 172, 139, 70, 113, 68, 100, 36, 95, 104, 12, 123, 134]
 print(sln.subArraySum(arr, n, s))
-
-class Solution1:
-    def duplicates(self, arr, n): 
-    	# code here
-        arr2 = []
-        arr3 = []
-        for num in arr:
-            if len(arr2) == 0:
-                arr2.append(num)
-            elif num in arr2:
-                if num not in arr3:
-                    arr3.append(num)
-            else:
-                arr2.append(num)
-        if len(arr3) == 0:
-            return [-1]
-        else:
-            arr3.sort(reverse=False)
-            return arr3
