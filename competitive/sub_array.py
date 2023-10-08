8fec29776edbf1756ef11e38488895df87656427
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-10-08 22:27:40 +0545

    sub array in the given array that sums up to given sum

--- competitive/sub_array.py
+++ competitive/sub_array.py
@@ -25,49 +25,42 @@ print(sln1.subArraySum(arr, n, s))
 # Complete solution
 #Function to find a continuous sub-array which adds up to a given number.
 class Solution:
-    def subArraySum(self,arr, n, sum_): 
-        # Initialize curr_sum as
-        # value of first element
+    def subArraySum(self,arr, n, sum_given): 
+        # Initialize curr_sum as value of first element
         # and starting point as 0 
         A = []
         curr_sum = arr[0]
         start = 0
 
-        # Add elements one by 
-        # one to curr_sum and 
-        # if the curr_sum exceeds 
-        # the sum, then remove 
-        # starting element 
+        # Add elements one by one to curr_sum and if the curr_sum exceeds 
+        # the sum, then remove starting element 
         i = 1
         while i <= n:
         
-            # If curr_sum exceeds
-            # the sum, then remove
+            # If curr_sum exceeds the sum, then remove
             # the starting elements
-            while curr_sum > sum_ and start < i-1:
-        
-                curr_sum = curr_sum - arr[start]
+            while curr_sum > sum_given and start < i-1:
+                curr_sum -= arr[start] # slower than curr_sum = curr_sum + arr[start]
                 start += 1
             
-            # If curr_sum becomes
-            # equal to sum, then
-            # return true
-            if curr_sum == sum_:
+            # If curr_sum becomes equal to sum, then return true
+            if curr_sum == sum_given:
                 A.append(start+1)
                 A.append(i)
                 return A
 
-            # Add this element 
-            # to curr_sum
+            # Add this element to curr_sum
             if i < n:
-                curr_sum = curr_sum + arr[i]
+                curr_sum += arr[i] # slower than curr_sum = curr_sum + arr[i]
             i += 1
 
-        # If we reach here, 
-        # then no subarray
+        # If we reach here, then no subarray
         A.append(-1)
         return A
 
 sln = Solution()
+arr = [45]
+n = len(arr)
+s = 45
 print("The complete solution : ")
 print(sln.subArraySum(arr, n, s))
