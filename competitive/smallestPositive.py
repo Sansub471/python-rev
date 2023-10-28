d314c0acf26590b25fcd156e67cafe3032e8a398
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-10-28 23:36:41 +0545

    half done, still confused, might see the total solution.

--- competitive/smallestPositive.py
+++ competitive/smallestPositive.py
@@ -1,5 +1,7 @@
 # You are given an array arr[] of N integers. The task is to find the smallest positive number missing from the array.
 
+# My assumption is the numbers will not be larger than N ????
+
 # Note: Positive number starts from 1.
 
 # Example 1:
@@ -19,3 +21,17 @@
 # Explanation: Smallest positive missing 
 # number is 2.
 
+def smallestPositiveNumber(arr, n):
+    # step 1 : bring -ve and zero elements to the left, and rest to the right
+    low = 0
+    for i in range(0, len(arr)):
+        if arr[i] <= 0:
+            arr[low], arr[i] = arr[i], arr[low]
+            low += 1
+    # step 2 : the positive part starts at index 'low' to the end
+    pos_len = len(arr[low:])
+
+
+arr = [1,2,3,4,5]
+n = len(arr)
+print(smallestPositiveNumber(arr, n))
