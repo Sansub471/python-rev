f07d27b8e2090293e8d92d3568a4646597e53fe5
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-10-30 23:51:53 +0545

    Parenthesis checker, or balanced expression.

---
+++ competitive/parenthesisChecker.py
@@ -0,0 +1,54 @@
+# Given an expression string x. Examine whether the pairs and the orders of {,},(,),[,] are correct in exp.
+# For example, the function should return 'true' for exp = [()]{}{[()()]()} and 'false' for exp = [(]).
+
+# Note: The drive code prints "balanced" if function return true, otherwise it prints "not balanced".
+
+# Example 1:
+
+# Input:
+# {([])}
+# Output: 
+# true
+# Explanation: 
+# { ( [ ] ) }. Same colored brackets can form 
+# balanced pairs, with 0 number of 
+# unbalanced bracket.
+# Example 2:
+
+# Input: 
+# ()
+# Output: 
+# true
+# Explanation: 
+# (). Same bracket can form balanced pairs, 
+# and here only 1 type of bracket is 
+# present and in balanced way.
+# Example 3:
+
+# Input: 
+# ([]
+# Output: 
+# false
+# Explanation: 
+# ([]. Here square bracket is balanced but 
+# the small bracket is not balanced and 
+# Hence , the output will be unbalanced.
+
+def ispar(x):
+    stack = []
+    for char in x:
+        if char == '{' or char == '[' or char == '(':
+            stack.append(char)
+        elif char == '}' or char == ']' or char == ')':
+            if len(stack) == 0:
+                return False
+            else:
+                p = stack[-1]
+                if p == '(' and char == ')' or p == '{' and char == '}' or p == '[' and char == ']':
+                    stack.pop()
+                else:
+                    return False
+    return True if len(stack) == 0 else False
+
+x = ')'
+print(ispar(x))
