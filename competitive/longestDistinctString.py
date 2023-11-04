54c2c1ded6d57883cf43f791480d48331cbd356f
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-11-04 23:50:44 +0545

    This problem is not solvable using simple technique, come back letter.

---
+++ competitive/longestDistinctString.py
@@ -0,0 +1,19 @@
+# Given a string S, find the length of the longest substring with all distinct characters. 
+
+# Example 1:
+
+# Input:
+# S = "geeksforgeeks"
+# Output: 7
+# Explanation: "eksforg" is the longest 
+# substring with all distinct characters.
+# Example 2:
+
+# Input: 
+# S = "aaa"
+# Output: 1
+# Explanation: "a" is the longest substring 
+# with all distinct characters.
+
+def longestSubstrDistinctChars(S):
+    pass
