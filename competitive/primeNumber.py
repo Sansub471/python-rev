bedbc57488bac0b1782d12fcf72c37c8b0583297
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-11-10 21:52:55 +0545

    isPrime function changed.

--- competitive/primeNumber.py
+++ competitive/primeNumber.py
@@ -1,12 +1,15 @@
 def isPrime (N):
     if(N == 1): # if N = 1 return 0
-        return 0
+        return False
     i = 2
     # loop from 2 to sqrt(N)
     while(i*i<=N): 
         if(N%i==0): # if N is divisble by any other number return 0 
-            return 0
+            return False
         i+=1
-    return 1 # return 1 if N is not divisible by any other number.
+    return True # return 1 if N is not divisible by any other number.
 
 # The factors from 2 to sqrt(n) have multiples from sqrt(n)+1 to n.
+
+
+print(isPrime(23))
