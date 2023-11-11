054c56f2f792e119b6caaf3109bdffae5d9ef126
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-11-11 13:46:38 +0545

    A number in power of X to Y.

---
+++ competitive/powerOfXtoY.py
@@ -0,0 +1,22 @@
+# Given a positive integer N, find if it can be expressed as xy where y > 1 and x > 0 and x and y both are both integers.
+
+# Example 1:
+
+# Input:
+# N = 8
+# Output:
+# 1
+# Explanation:
+# 8 can be expressed
+# as 23.
+
+# Example 2:
+
+# Input:
+# N = 7
+# Output:
+# 0
+# Explanation:
+# 7 can't be expressed
+# int the form xy.
+
