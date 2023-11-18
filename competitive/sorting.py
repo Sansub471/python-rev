1a68172581720edd79e17e1d4785de579b293dd5
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-11-18 12:22:28 +0545

    Sorting from memory, time complexity O(n^2)

---
+++ competitive/sorting.py
@@ -0,0 +1,11 @@
+# Let's try a sorting technique from out of memory
+
+arr = [20,9,3,2,1]
+def sorting(arr):
+    for i in range(0, len(arr)):
+        for j in range(i, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+
+print(sorting(arr))
